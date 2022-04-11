76c155d6d930c65d1301c2b05781e808ea82f82e
zhaoruiyang98 <zhaoruiyang19@mails.ucas.edu.cn>
2022-04-11 20:04:25 +0800

    fix(test): add PathContext

--- tests/func/test_likelihood.py
+++ tests/func/test_likelihood.py
@@ -1,16 +1,14 @@
-import os
 import cobaya
 from pathlib import Path
+from eftpipe.tools import PathContext
 
 def test_EFTLike():
     current = Path('.').cwd()
     yaml_file = current / "tests" / "yamls" / "mock_LRG_ELG_x_NGC_km0p15_fix_proposal.yaml"
-    os.chdir(current / 'cobaya')
 
-    model = cobaya.get_model(yaml_file, debug=True)
+    with PathContext("cobaya"):
+        model = cobaya.get_model(yaml_file, debug=True)
     point = model.prior.sample(ignore_external=True)[0]
     params_name = model.parameterization.sampled_params().keys()
     point_dct = {key: value for key, value in zip(params_name, point)}
     out = model.logpost(point_dct)
-
-    os.chdir(current)
