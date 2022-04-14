cff74824b73641c54c3b199e8d8a2404bf25bd09
zhaoruiyang98 <zhaoruiyang19@mails.ucas.edu.cn>
2022-04-14 19:47:57 +0800

    test(interface): compare CobayaCamb with CobayaClassy

--- tests/compare/test_interface.py
+++ tests/compare/test_interface.py
@@ -1,7 +1,10 @@
 import numpy as np
 import cobaya
+import pytest
+from pathlib import Path
 from eftpipe.interface import CambProvider
 from eftpipe.interface import CobayaCambProvider
+from eftpipe.interface import CobayaClassyProvider
 
 
 def two_providers():
@@ -116,4 +119,74 @@ def test_CobayaCambProvider(compare_ndarrays):
             'fsigma8': {'atol': 0, 'rtol': 1e-4},
             'sigma8': {'atol': 0, 'rtol': 1e-4}
         },
+    )
+
+
+@pytest.mark.fcompare
+def test_compare_camb_and_classy(
+    compare_ndarrays, rtol: float, atol: float, yamlroot: Path
+):
+    camb_model = cobaya.get_model(yamlroot / 'camb_planck18.yaml')
+    classy_model = cobaya.get_model(yamlroot / 'classy_planck18.yaml')
+
+    z = 0.5
+
+    camb_model.add_requirements({
+        "Pk_grid": {
+            "nonlinear": False,
+            "z": [z],
+            "k_max": 5,
+        },
+        "Hubble": {'z': [0., z]},
+        "angular_diameter_distance": {'z': [z]},
+        "fsigma8": {'z': [z]},
+        "sigma8_z": {'z': [z]},
+        "rdrag": None,
+        'sigma8': None,
+    })
+    classy_model.add_requirements({
+        "Pk_interpolator": {
+            "nonlinear": False,
+            "z": [z],
+            "k_max": 5,
+        },
+        "Hubble": {'z': [0., z]},
+        "angular_diameter_distance": {'z': [z]},
+        "rdrag": None,
+        'sigma8': None,
+    })
+
+    camb_point = {
+        "logA": 3.05,
+        "ombh2": 0.02214,
+        "omch2": 0.12,
+    }
+    classy_point = {
+        "logA": 3.05,
+        "omega_b": 0.02214,
+        "omega_cdm": 0.12,
+    }
+
+    camb_model.logpost(camb_point)
+    classy_model.logpost(classy_point)
+
+    camb_provider = CobayaCambProvider(camb_model.provider, z)
+    classy_provider = CobayaClassyProvider(classy_model.provider, z)
+
+    dcts = []
+    for provider in (classy_provider, camb_provider):
+        kh = np.logspace(-4, 0, 200)
+        dct = {
+            'rdrag': provider.get_rdrag(),
+            'DA': provider.get_DA(z),
+            'H': provider.get_H(z),
+            'f': provider.get_f(z),
+            'sigma8': provider.provider.get_param('sigma8'),
+            'pkh': provider.interp_pkh(kh),
+        }
+        dcts.append(dct)
+    ref, data = dcts
+    compare_ndarrays(
+        ref, data,
+        default_tolerance={'atol': atol, 'rtol': rtol},
     )
