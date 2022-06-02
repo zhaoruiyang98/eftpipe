c167a4ba348e539a9c6098014daa849281f4fca5
zhaoruiyang98 <zhaoruiyang19@mails.ucas.edu.cn>
2022-06-02 16:03:52 +0800

    bump to python=3.8 and cobaya=3.2.1

--- eftpipe/__init__.py
+++ eftpipe/__init__.py
@@ -4,5 +4,5 @@ from eftpipe.pybird import pybird
 from eftpipe.log import logger_setup
 
 __author__ = "Ruiyang Zhao and Xiaoyong Mu"
-__version__ = '0.0.1'
-__url__ = 'https://github.com/zhaoruiyang98/eftpipe'
+__version__ = "0.0.1"
+__url__ = "https://github.com/zhaoruiyang98/eftpipe"
--- setup.py
+++ setup.py
@@ -1,37 +1,41 @@
-import sys
+import re
 from setuptools import setup, find_packages
 from pathlib import Path
 
 
+def find_version():
+    contents = open(Path(__file__).parent / "eftpipe" / "__init__.py").read()
+    pattern = re.compile(r"__version__ = \"(.*?)\"")
+    match = pattern.search(contents)
+    if match:
+        return match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 def read_long_description():
-    with open(Path(__file__).parent / 'README.md', encoding='utf-8') as f:
+    with open(Path(__file__).parent / "README.md", encoding="utf-8") as f:
         text = f.read()
     return text
 
 
-if sys.version_info >= (3, 8):
-    extra_install = []
-else:
-    extra_install = ['typing_extensions>=3.10']
-
 install_requires = [
-    "numpy>=1.20",
+    "numpy>=1.21",
     "scipy>=1.6",
-    "cobaya>=3.1.1",
+    "cobaya>=3.2.1",
+    "typing_extensions>=4.1,<5",
 ]
-install_requires += extra_install
 
-exclude_dir = ['docs', 'tests', 'notes', 'cobaya']
+exclude_dir = ["docs", "tests", "notes", "cobaya"]
 packages = find_packages(exclude=exclude_dir)
 
 test_requires = [
-    'pytest>=6.0', 'pytest-cov',
-    'pytest-regressions>=2.3.0', 'pytest-datadir',
+    "pytest>=6.0",
+    "pytest-cov",
+    "pytest-regressions>=2.3.0",
+    "pytest-datadir",
 ]
 
-gui_requires = [
-    'matplotlib', 'getdist'
-]
+gui_requires = ["matplotlib", "getdist"]
 
 classifiers = [
     "Development Status :: 3 - Alpha",
@@ -39,37 +43,35 @@ classifiers = [
     "Intended Audience :: Science/Research",
     "License :: OSI Approved :: MIT License",
     "Operating System :: OS Independent",
-    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Topic :: Scientific/Engineering :: Astronomy",
     "Typing :: Typed",
 ]
 
 setup(
-    name='eftpipe',
-    version='0.0.1',
-    description='EFTofLSS analysis pipeline for cross power spectrum',
+    name="eftpipe",
+    version=find_version(),
+    description="EFTofLSS analysis pipeline",
     long_description=read_long_description(),
-    url='https://github.com/zhaoruiyang98/eftpipe',
+    url="https://github.com/zhaoruiyang98/eftpipe",
     project_urls={
-        'Source': 'https://github.com/zhaoruiyang98/eftpipe',
-        'Tracker': 'https://github.com/zhaoruiyang98/eftpipe/issues',
-        'Licensing': 'https://github.com/zhaoruiyang98/eftpipe/blob/main/LICENSE'
+        "Source": "https://github.com/zhaoruiyang98/eftpipe",
+        "Tracker": "https://github.com/zhaoruiyang98/eftpipe/issues",
+        "Licensing": "https://github.com/zhaoruiyang98/eftpipe/blob/main/LICENSE",
     },
-    author='Ruiyang Zhao and Xiaoyong Mu',
-    author_email='zhaoruiyang19@mails.ucas.edu.cn',
-    license='MIT',
-    python_requires='>=3.7, <3.10',
-    keywords='cosmology perturbation EFT',
+    author="Ruiyang Zhao and Xiaoyong Mu",
+    author_email="zhaoruiyang19@mails.ucas.edu.cn",
+    license="MIT",
+    python_requires=">=3.8",
+    keywords="cosmology perturbation EFT",
     packages=packages,
     install_requires=install_requires,
     extras_require={
-        'test': test_requires,
-        'gui': gui_requires,
-        'dev': test_requires + gui_requires + ['pyyaml>=5.1', 'line_profiler']
+        "test": test_requires,
+        "gui": gui_requires,
+        "dev": test_requires + gui_requires + ["pyyaml>=5.1", "line_profiler"],
     },
-    package_data={
-        'eftpipe': ['*.yaml', '*.bibtex', 'py.typed']
-    },
+    package_data={"eftpipe": ["*.yaml", "*.bibtex", "py.typed"]},
     classifiers=classifiers,
     zip_safe=False,
 )
