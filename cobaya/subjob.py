c02d5bb124da9a36113370d4f1b1c0b4fa3b6229
zhaoruiyang98 <zhaoruiyang19@mails.ucas.edu.cn>
2023-01-20 14:22:25 +0000

    feat(subjob): record last-failed

--- cobaya/subjob.py
+++ cobaya/subjob.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 import argparse
 import subprocess
 import time
+import yaml
 from pathlib import Path
 from cobaya.yaml import yaml_dump_file
 from cobaya.yaml import yaml_load_file
@@ -56,7 +57,7 @@ def collect_jobs(files: list[str]) -> set[Path]:
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("mode", choices=["mcmc", "minimize", "evaluate"])
-    parser.add_argument("files", nargs="+", help="yaml files, support glob")
+    parser.add_argument("files", nargs="*", help="yaml files, support glob")
     parser.add_argument("--outdir", type=Path, default=None, help="output folder")
     parser.add_argument("--local", action="store_true", help="do not submit job")
     parser.add_argument(
@@ -64,33 +65,48 @@ def main():
     )
     parser.add_argument("--Rminus1_stop", type=float, default=0.005)
     parser.add_argument("--max_tries", type=int, default=100000)
+    parser.add_argument(
+        "-lf", "--last_failed", action="store_true", help="resume last failed jobs"
+    )
     group = parser.add_mutually_exclusive_group()
     group.add_argument("-r", action="store_true", help="resume")
     group.add_argument("-f", action="store_true", help="force overwrite")
     args = parser.parse_args()
 
-    for job in collect_jobs(args.files):
-        if args.mode == "minimize":
-            reformat_minimize(job, args.outdir)
-        elif args.mode == "evaluate":
-            reformat_evaluate(job, args.outdir)
-        else:
-            reformat_mcmc(
-                job,
-                args.outdir,
-                Rminus1_stop=args.Rminus1_stop,
-                max_tries=args.max_tries,
-            )
+    jobqueue = collect_jobs(args.files)
+    if args.last_failed and (cache_path := Path(".subjob.cache")).exists():
+        with cache_path.open() as f:
+            last_failed = yaml.safe_load(f)
+        jobqueue.update(last_failed)
+    remains = jobqueue.copy()
+    try:
+        for job in jobqueue:
+            if args.mode == "minimize":
+                reformat_minimize(job, args.outdir)
+            elif args.mode == "evaluate":
+                reformat_evaluate(job, args.outdir)
+            else:
+                reformat_mcmc(
+                    job,
+                    args.outdir,
+                    Rminus1_stop=args.Rminus1_stop,
+                    max_tries=args.max_tries,
+                )
 
-        command = ["cobaya-run" if args.local else "cobaya-run-job", str(job)]
-        if args.r:
-            command += ["-r"]
-        elif args.f:
-            command += ["-f"]
+            command = ["cobaya-run" if args.local else "cobaya-run-job", str(job)]
+            if args.r:
+                command += ["-r"]
+            elif args.f:
+                command += ["-f"]
 
-        print(" ".join(command))
-        subprocess.run(command)
-        time.sleep(args.wait)
+            print(" ".join(command))
+            subprocess.run(command)
+            remains.remove(job)
+            time.sleep(args.wait)
+    finally:
+        if remains:
+            with Path(".subjob.cache").open("w") as f:
+                yaml.safe_dump(list(remains), f)
 
 
 if __name__ == "__main__":
