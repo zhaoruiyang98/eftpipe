388f562c6e4bc183da606e59b5c5a3e01283270d
zhaoruiyang98 <zhaoruiyang19@mails.ucas.edu.cn>
2023-09-01 09:53:33 +0100

    feat(binning): more accurate binning

--- eftpipe/binning.py
+++ eftpipe/binning.py
@@ -47,6 +47,9 @@ class Binning(BirdTransformer, HasLogger):
         decimals: int = 2,
         co: pybird.Common = pybird.common,
         name: str = "pybird.binning",
+        kstart: float | None = None,
+        kend: float | None = None,
+        nbins: int | None = None,
     ) -> None:
         self.set_logger(name=name)
         self.kout = np.array(kout)
@@ -60,37 +63,54 @@ class Binning(BirdTransformer, HasLogger):
             self.kout[0],
             self.kout[-1],
         )
-        # XXX: only suitable for dk like: 0.01, 0.005, 0.001, ...
-        kspaces = np.around(self.kout[1:] - self.kout[:-1], decimals=decimals)  # type: ignore
-        kspace_diff = kspaces[1:] - kspaces[:-1]
-        if not np.allclose(kspace_diff, 0, rtol=0, atol=1e-6):
-            self.mpi_warning(
-                "binning correction on, "
-                "but given kout seems not linearly spaced, "
-                "be careful because the constructed kbins may be wrong, "
-                "especially when 'kmax' is small",
+        if kstart is None and kend is None and nbins is None:
+            # XXX: only suitable for dk like: 0.01, 0.005, 0.001, ...
+            kspaces = np.around(self.kout[1:] - self.kout[:-1], decimals=decimals)  # type: ignore
+            kspace_diff = kspaces[1:] - kspaces[:-1]
+            if not np.allclose(kspace_diff, 0, rtol=0, atol=1e-6):
+                self.mpi_warning(
+                    "binning correction on, "
+                    "but given kout seems not linearly spaced, "
+                    "be careful because the constructed kbins may be wrong, "
+                    "especially when 'kmax' is small",
+                )
+            self.mpi_info(
+                "round the difference of last two kout to %d decimal places",
+                self.decimals,
             )
-        self.load_binning(self.kout)
-        self.mpi_info("num of kgrids in each bin: %d", self.points[0].size)
-        self.mpi_info(
-            "round the difference of last two kout to %d decimal places",
-            self.decimals,
-        )
-        self.mpi_info(
-            "inferred kedges=%f, %f, ..., %f",
-            self.binmin[0],
-            self.binmin[1],
-            self.binmax[-1],
-        )
+            self.load_binning(self.kout)
+            self.mpi_info(
+                "inferred kedges=%f, %f, ..., %f",
+                self.binmin[0],
+                self.binmin[1],
+                self.binmax[-1],
+            )
+        else:
+            if kstart is None or kend is None or nbins is None:
+                raise ValueError("need specify kstart, kend and nbins together")
+            kedges = np.linspace(kstart, kend, nbins + 1)
+            ileft = np.searchsorted(kedges, self.kout[0]) - 1
+            iright = np.searchsorted(kedges, self.kout[-1], side="right") + 1
+            kedges = kedges[ileft:iright]
+            self.load_binning(self.kout, kedges=kedges)
+            self.mpi_info(
+                "kedges: %.3f, %.3f, ..., %.3f", kedges[0], kedges[1], kedges[-1]
+            )
 
-    def load_binning(self, setkout) -> None:
+        self.mpi_info("num of kgrids in each bin: %d", self.points[0].size)
+
+    def load_binning(self, setkout, kedges=None) -> None:
         """
         Create the bins of the data k's
         """
-        delta_k = np.round(setkout[-1] - setkout[-2], self.decimals)
-        kcentral = (setkout[-1] - delta_k * np.arange(len(setkout)))[::-1]
-        self.binmin = binmin = kcentral - delta_k / 2
-        self.binmax = binmax = kcentral + delta_k / 2
+        if kedges is None:
+            delta_k = np.round(setkout[-1] - setkout[-2], self.decimals)
+            kcentral = (setkout[-1] - delta_k * np.arange(len(setkout)))[::-1]
+            self.binmin = binmin = kcentral - delta_k / 2
+            self.binmax = binmax = kcentral + delta_k / 2
+        else:
+            self.binmin = binmin = kedges[:-1]
+            self.binmax = binmax = kedges[1:]
         self.binvol = np.array(
             [
                 quad(lambda k: k**2, kbinmin, kbinmax)[0]
